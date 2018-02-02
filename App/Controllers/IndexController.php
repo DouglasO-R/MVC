14e84d72d345e689a84dfac87bf2ac18b02836ed
DouglasOR <douglasor@outlook.com>
2018-02-01 23:14:55 -0200

    rotas

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+class IndexController
+{
+
+    public function index()
+    {
+
+        echo "Rota: / controller: index";
+
+    }
+
+    public function contato()
+    {
+
+        echo"Rota : /contato  controller: contato";
+    }
+
+}
